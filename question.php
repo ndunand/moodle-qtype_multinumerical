6a01ea712f0b46bab7e0bc966e9f1f92a5cd8d75
Nicolas Alexandropoulos <Nicolas.Alexandropoulos@unil.ch>
2023-01-06 13:26:02 +0100

    reduce cyclomatic complexity, fix deprecated calls, fix uninitialized variables

--- question.php
+++ question.php
@@ -75,7 +75,6 @@ class qtype_multinumerical_question extends question_graded_automatically {
     	global $CFG;
     	$score = 0;
     	$feedbackperconditions = $this->get_feedbackperconditions();
-    	$conditionsfullfilled = array();
     	$feedbackperconditions_computed = array();
      	$conditions = $this->get_conditions();
     	foreach ($conditions as $conditionid => $condition) {
@@ -83,8 +82,7 @@ class qtype_multinumerical_question extends question_graded_automatically {
     		$values = '';
     		if ($this->check_condition($condition, $values, $response)) {
     			$score++;
-    			$conditionsfullfilled[] = 1;
-    			if (strlen(trim($feedbackforthiscondition[0])) > 0) {
+    			if (trim($feedbackforthiscondition[0]) !== '') {
     				if ($this->usecolorforfeedback) {
 	    				$feedbackperconditions_computed[$conditionid] = '<span style="color:#090">';
 	    				$feedbackperconditions_computed[$conditionid] .= (preg_match('/(usepackage{color})/', get_config('filter_tex', 'latexpreamble'))) ? (preg_replace('/(.*)\$\$(.*)\$\$(.*)/', '${1}\$\$\\textcolor{green}{${2}}\$\$${3}', $feedbackforthiscondition[0])) : ($feedbackforthiscondition[0]);
@@ -98,22 +96,19 @@ class qtype_multinumerical_question extends question_graded_automatically {
     				unset($feedbackperconditions[$conditionid]);
     			}
     		}
-    		else {
-    			$conditionsfullfilled[] = 0;
-    			if (isset($feedbackforthiscondition[1]) && strlen(trim($feedbackforthiscondition[1])) > 0) {
-    				if ($this->usecolorforfeedback) {
-	    				$feedbackperconditions_computed[$conditionid] = '<span style="color:#f00">';
-	    				$feedbackperconditions_computed[$conditionid] .= (preg_match('/(usepackage{color})/', get_config('filter_tex', 'latexpreamble'))) ? (preg_replace('/(.*)\$\$(.*)\$\$(.*)/', '${1}\$\$\\textcolor{red}{${2}}\$\$${3}', $feedbackforthiscondition[1])) : ($feedbackforthiscondition[1]);
-	    				$feedbackperconditions_computed[$conditionid] .= '</span>';
-    				}
-    				else {
-    					$feedbackperconditions_computed[$conditionid] = $feedbackforthiscondition[1];
-    				}
-    			}
-    			else {
-    				unset($feedbackperconditions[$conditionid]);
-    			}
-    		}
+    		elseif (isset($feedbackforthiscondition[1]) && trim($feedbackforthiscondition[1]) !== '') {
+                if ($this->usecolorforfeedback) {
+                    $feedbackperconditions_computed[$conditionid] = '<span style="color:#f00">';
+                    $feedbackperconditions_computed[$conditionid] .= (preg_match('/(usepackage{color})/', get_config('filter_tex', 'latexpreamble'))) ? (preg_replace('/(.*)\$\$(.*)\$\$(.*)/', '${1}\$\$\\textcolor{red}{${2}}\$\$${3}', $feedbackforthiscondition[1])) : ($feedbackforthiscondition[1]);
+                    $feedbackperconditions_computed[$conditionid] .= '</span>';
+                }
+                else {
+                    $feedbackperconditions_computed[$conditionid] = $feedbackforthiscondition[1];
+                }
+            }
+            else {
+                unset($feedbackperconditions[$conditionid]);
+            }
     		if ($this->displaycalc && isset($feedbackperconditions[$conditionid]) && (!preg_match('/^\s*([A-Za-z]+\d*)\s*[=|<|>].*$/', $condition, $matches) || $this->displaycalc == 1)) {
     		    $feedbackperconditions_computed[$conditionid] .= '<ul><li>'.$values.'</li></ul>';
     		}
@@ -129,6 +124,8 @@ class qtype_multinumerical_question extends question_graded_automatically {
         $values = '';
         $interval = false;
         $operators = array('<=', '>=', '<', '>', '=');
+        $left = null;
+        $right = null;
         // careful with operators relative positions here, see following foreach()
         foreach ($operators as $operator) {
             $operatorposition = strpos($condition, $operator);
@@ -149,24 +146,24 @@ class qtype_multinumerical_question extends question_graded_automatically {
             $value = str_replace(' ', '', $value);
             $value = str_replace($thousandssep, '', $value);
             $value = str_replace($decsep, '.', $value);
-            $value = floatval($value);
-        	// EvalMath doesn't like uppercase variable names
-        	$math->evaluate(strtolower(substr($param, 7)).'='.$value);
+            $value = (float)$value;
+            // EvalMath doesn't like uppercase variable names
+            $math->evaluate(strtolower(substr($param, 7)).'='.$value);
         }
         $leftvalue = $math->evaluate($left);
-        if ($operator == '=') {
+        if ($operator === '=') {
             $operator = '==';
             $matches = array();
             if (preg_match('/^\s*([A-Z]*[a-z]*\w*)\s*=\s*([\[|\]])(.+);(.+)([\[|\]])$/', $condition, $matches)) {
                 // we're dealing with an interval
                 $interval = true;
                 $operator = '';
-                $rightvalue = ($matches[2] == "[") ? (">=") : (">");
+                $rightvalue = ($matches[2] === "[") ? (">=") : (">");
                 $val1 = (float)$math->evaluate(strtolower($matches[3]));
                 $val2 = (float)$math->evaluate(strtolower($matches[4]));
                 // failsafe : EvalMath returns false instead of zero, so cast result as float
                 $rightvalue .= $val1 . " && " . $leftvalue;
-                $rightvalue .= ($matches[5] == "]") ? ("<=") : ("<");
+                $rightvalue .= ($matches[5] === "]") ? ("<=") : ("<");
                 $rightvalue .= $val2;
             }
         }
@@ -177,14 +174,13 @@ class qtype_multinumerical_question extends question_graded_automatically {
         else {
             $values .= $leftvalue . ' = ' . $matches[2] . number_format($val1, 3, $decsep, $thousandssep) . ';' . number_format($val2, 3, $decsep, $thousandssep) . $matches[5];
         }
-    	if (strlen($leftvalue) > 0 && isset($operator) && strlen($rightvalue) > 0 && eval('return('.$leftvalue.$operator.$rightvalue.');')) {
-    	    $valuesspan = '<span';
-    	    $valuesspan .= ($this->usecolorforfeedback) ? (' style="color:#090"') : ('');
-    	    $valuesspan .= '>'.get_string('conditionverified', 'qtype_multinumerical').' : '.$values.'</span>';
-    	    $values = $valuesspan;
-          return true;
-        }
         $valuesspan = '<span';
+        if ($leftvalue != '' && isset($operator) && $rightvalue != '' && eval('return('.$leftvalue.$operator.$rightvalue.');')) {
+            $valuesspan .= ($this->usecolorforfeedback) ? (' style="color:#090"') : ('');
+            $valuesspan .= '>'.get_string('conditionverified', 'qtype_multinumerical').' : '.$values.'</span>';
+            $values = $valuesspan;
+            return true;
+        }
         $valuesspan .= ($this->usecolorforfeedback) ? (' style="color:#f00"') : ('');
         $valuesspan .= '>'.get_string('conditionnotverified', 'qtype_multinumerical').' : '.$values.'</span>';
         $values = $valuesspan;
@@ -217,20 +213,22 @@ class qtype_multinumerical_question extends question_graded_automatically {
     }
 
     public function check_file_access($qa, $options, $component, $filearea,
-            $args, $forcedownload) {
-        if ($component == 'question' && $filearea == 'answerfeedback') {
+        $args, $forcedownload) {
+        if ($component === 'question' && $filearea === 'answerfeedback') {
             $currentanswer = $qa->get_last_qt_var('answer');
             $answer = $qa->get_question()->get_matching_answer(array('answer' => $currentanswer));
             $answerid = reset($args); // itemid is answer id.
             return $options->feedback && $answerid == $answer->id;
 
-        } else if ($component == 'question' && $filearea == 'hint') {
+        }
+
+        if ($component === 'question' && $filearea === 'hint') {
             return $this->check_hint_file_access($qa, $options, $args);
 
-        } else {
-            return parent::check_file_access($qa, $options, $component, $filearea,
-                    $args, $forcedownload);
         }
+
+        return parent::check_file_access($qa, $options, $component, $filearea,
+                                         $args, $forcedownload);
     }
 
     public function get_correct_response(){
@@ -243,8 +241,8 @@ class qtype_multinumerical_question extends question_graded_automatically {
     }
 
     public function grade_response(array $response){
-    	$score = $this->compute_feedbackperconditions($response);
-        $fraction = $score / sizeof($this->get_conditions());
+        $score = $this->compute_feedbackperconditions($response);
+        $fraction = $score / count($this->get_conditions());
         if ($this->binarygrade) {
             $fraction = floor($fraction);
         }
